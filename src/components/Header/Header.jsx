fcd72933017e3acf215f55cb74433ce7614d0b07
aman-atg <amanansari.a2@gmail.com>
2020-03-19 03:15:05 +0530

    add required classes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,18 +2,26 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="header">
-      <div className="header_Content">
-        <h2>More than just shorter links</h2>
-        Build your brand's recognition and get detailed insights on how your
-        links are performing.
-        <button>Get Started</button>
-      </div>
+    <div className="header wrapper">
       <div className="header_SVG">
         <img
           src="/images/illustration-working.svg"
           alt="A male working in front of computer"
         />
+      </div>
+      <div className="header_Content">
+        <div className="header_Content_Wrapper">
+          <h2>More than just shorter links</h2>
+          <p>
+            Build your brand's recognition and get detailed insights on how your
+            links are performing.
+          </p>
+          <div className="header_Get-started">
+            <a className="primary-btn btn--lg" href="#">
+              Get Started
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
