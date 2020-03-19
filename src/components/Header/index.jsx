3627280d5b1c778b72dbc47e672d4e4608157187
aman-atg <amanansari.a2@gmail.com>
2020-03-19 12:03:04 +0530

    add ShortenLnk

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Header from "./Header";
+import ShortenLnk from "./ShortenLnk";
 const HeaderIndex = () => {
-  return <Header />;
+  return (
+    <Fragment>
+      <Header />
+      <ShortenLnk />
+    </Fragment>
+  );
 };
 
 export default HeaderIndex;
