b7ec1a192cc74b9ace2275ed0b0d12a908149770
aman-atg <amanansari.a2@gmail.com>
2020-03-19 22:02:53 +0530

    render Cards comp.

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Cards from "./Cards";
 const Body = () => {
   return (
     <div className="body wrapper">
@@ -11,6 +11,8 @@ const Body = () => {
             statistics dashboard.
           </p>
         </div>
+
+        <Cards />
       </div>
     </div>
   );
