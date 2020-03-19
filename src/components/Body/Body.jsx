f0cb660fbc280d7d0cf1be25547ebba23294fd4b
aman-atg <amanansari.a2@gmail.com>
2020-03-19 18:48:53 +0530

    body headline finish it

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Body = () => {
+  return (
+    <div className="body wrapper">
+      <div className="body_Box">
+        <div className="body_Box_Fixed">
+          <h1>Advanced Statistics</h1>
+          <p>
+            Track how your links are performing across the web with our advanced
+            statistics dashboard.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Body;
