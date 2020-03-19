6d551db3868072659a8226e90c7fe1003183e892
aman-atg <amanansari.a2@gmail.com>
2020-03-19 23:28:56 +0530

    complete Card comp. as of now

--- src/components/Body/Card.jsx
+++ src/components/Body/Card.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Proptypes from "prop-types";
 const Card = props => {
-  const { logo, heading, content } = props;
+  const { logo, heading, content, cls } = props;
 
   return (
-    <div className="card">
+    <div className={`card ${cls}`}>
       <img className="logo" src={`/images/${logo}`} alt={`${heading}`} />
       <div className="card_Body ">
         <h2 className="heading"> {heading} </h2>
