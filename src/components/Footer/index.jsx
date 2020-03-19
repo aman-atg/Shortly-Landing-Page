dd6930ac03cdb9b004e22b90710ea8e390692c97
aman-atg <amanansari.a2@gmail.com>
2020-03-20 00:34:36 +0530

    index file for footer done

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FooterHeading from "./FooterHeading";
+import Footer from "./Footer";
+
+const FooterIndex = () => {
+  return (
+    <div className="footerIndex">
+      <FooterHeading />
+      <Footer />
+    </div>
+  );
+};
+
+export default FooterIndex;
