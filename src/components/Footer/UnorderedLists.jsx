d07e0652a0ee62a236931855d996f2bf2c0bf340
aman-atg <amanansari.a2@gmail.com>
2020-03-20 02:46:45 +0530

    small change

--- src/components/Footer/UnorderedLists.jsx
+++ src/components/Footer/UnorderedLists.jsx
@@ -3,19 +3,19 @@ const UnorderedLists = () => {
   return (
     <div className="ul-lists">
       <ul className="ul-lists-1">
-        <li>Features</li>
+        <h4>Features</h4>
         <li>Link Shortening</li>
         <li>Branded Links</li>
         <li>Analytics</li>
       </ul>
       <ul className="ul-lists-2">
-        <li> Resources</li>
+        <h4> Resources</h4>
         <li> Blog</li>
         <li> Developers</li>
         <li> Support</li>
       </ul>
       <ul className="ul-lists-3">
-        <li> Company</li>
+        <h4> Company</h4>
         <li> About</li>
         <li> Our Team</li>
         <li> Careers</li>
