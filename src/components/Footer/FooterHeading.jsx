1ba2f306b4ba8aadd2ca9d6a553c51faef6325d8
aman-atg <amanansari.a2@gmail.com>
2020-03-20 00:34:21 +0530

    footerHeading done

---
+++ src/components/Footer/FooterHeading.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FooterHeading = () => {
+  return (
+    <div className="footerHeading">
+      <h1>Boost your links today</h1>
+      <span className="primary-btn btn--lg">Get Started</span>
+    </div>
+  );
+};
+
+export default FooterHeading;
