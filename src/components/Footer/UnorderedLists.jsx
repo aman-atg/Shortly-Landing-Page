a23e792002a2a1eb8903b6316538c636ca578116
aman-atg <amanansari.a2@gmail.com>
2020-03-20 02:11:47 +0530

    unorderedList complete

--- src/components/Footer/UnorderedLists.jsx
+++ src/components/Footer/UnorderedLists.jsx
@@ -3,10 +3,24 @@ const UnorderedLists = () => {
   return (
     <div className="ul-lists">
       <ul className="ul-lists-1">
-        <li>fda</li>
+        <li>Features</li>
+        <li>Link Shortening</li>
+        <li>Branded Links</li>
+        <li>Analytics</li>
       </ul>
-      <ul className="ul-lists-2"></ul>
-      <ul className="ul-lists-3"></ul>
+      <ul className="ul-lists-2">
+        <li> Resources</li>
+        <li> Blog</li>
+        <li> Developers</li>
+        <li> Support</li>
+      </ul>
+      <ul className="ul-lists-3">
+        <li> Company</li>
+        <li> About</li>
+        <li> Our Team</li>
+        <li> Careers</li>
+        <li> Contact</li>
+      </ul>
     </div>
   );
 };
