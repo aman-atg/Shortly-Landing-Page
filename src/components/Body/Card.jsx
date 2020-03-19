2354ab0c8c9840a6a68036f67213d8a2f3c3772c
aman-atg <amanansari.a2@gmail.com>
2020-03-19 22:03:08 +0530

    create Card comp.

---
+++ src/components/Body/Card.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Proptypes from "prop-types";
+const Card = props => {
+  const { logo, heading, content } = props;
+
+  return (
+    <div className="card">
+      <img className="logo" src={`/images/${logo}`} alt={`${heading}`} />
+      <div className="card_Body ">
+        <h2 className="heading"> {heading} </h2>
+        <p className="content"> {content} </p>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  logo: Proptypes.string.isRequired,
+  heading: Proptypes.string.isRequired,
+  content: Proptypes.string.isRequired
+};
+
+export default Card;
