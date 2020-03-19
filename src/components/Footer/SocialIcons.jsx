4a40a9457aef0274dd9e8105cdd4451a6f1fb8d5
aman-atg <amanansari.a2@gmail.com>
2020-03-20 02:46:33 +0530

    add socials icons

--- src/components/Footer/SocialIcons.jsx
+++ src/components/Footer/SocialIcons.jsx
@@ -4,10 +4,18 @@ const SocialIcons = () => {
   return (
     <div className="social-icons">
       <ul>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>
+          <img src="images/icon-facebook.svg" alt="facebook icon" />
+        </li>
+        <li>
+          <img src="images/icon-twitter.svg" alt="twitter icon" />
+        </li>
+        <li>
+          <img src="images/icon-pinterest.svg" alt="pinterest icon" />
+        </li>
+        <li>
+          <img src="images/icon-instagram.svg" alt="instagram icon" />
+        </li>
       </ul>
     </div>
   );
