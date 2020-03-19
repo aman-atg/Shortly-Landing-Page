289810838a0fff366b4526c45f7801e2773d584c
aman-atg <amanansari.a2@gmail.com>
2020-03-19 17:38:08 +0530

    add bodyIndex comp. to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavIndex from "./components/Navbar/";
 import HeaderIndex from "./components/Header";
+import BodyIndex from "./components/Body";
 
 const App = () => {
   return (
     <div className="app">
       <NavIndex />
       <HeaderIndex />
+      <BodyIndex />
     </div>
   );
 };
