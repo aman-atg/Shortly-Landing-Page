66d87aa8229b5216b95bb053971a4beffc056b7e
aman-atg <amanansari.a2@gmail.com>
2020-03-20 00:34:51 +0530

    setup done

---
+++ src/components/Footer/SocialIcons.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const SocialIcons = () => {
+  return (
+    <div className="social-icons">
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  );
+};
+
+export default SocialIcons;
---
+++ src/components/Footer/UnorderedLists.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const UnorderedLists = () => {
+  return (
+    <div className="ul-lists">
+      <ul className="ul-lists-1">
+        <li>fda</li>
+      </ul>
+      <ul className="ul-lists-2"></ul>
+      <ul className="ul-lists-3"></ul>
+    </div>
+  );
+};
+
+export default UnorderedLists;
