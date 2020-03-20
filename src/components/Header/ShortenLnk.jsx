496967e2a25cc15da95918f1bb878754e72d3405
aman-atg <amanansari.a2@gmail.com>
2020-03-20 15:42:39 +0530

    div changed to form and sfc changed to class comp

--- src/components/Header/ShortenLnk.jsx
+++ src/components/Header/ShortenLnk.jsx
@@ -1,21 +1,40 @@
 import React from "react";
+import Proptypes from "prop-types";
+//make it a class
+class ShortenLnk extends React.Component {
+  state = {
+    url: ""
+  };
 
-const ShortenLnk = () => {
-  return (
-    <div className="wrapper">
-      <div className="shorten-lnk box">
-        <div className="shorten-lnk_Input-group">
-          <input
-            type="text"
-            name="link"
-            id="link"
-            placeholder="Shorten a link here..."
-          />
-          <span className="primary-btn btn--lg btn--move">Shorten It!</span>
+  render() {
+    const { props, state } = this;
+
+    return (
+      <div className="wrapper">
+        <div className="shorten-lnk box">
+          <form
+            className="shorten-lnk_Input-group"
+            onSubmit={() => props.onLinkSubmit()}
+          >
+            <input
+              type="text"
+              name="link"
+              id="link"
+              placeholder="Shorten a link here..."
+            />
+            <span
+              onClick={x => props.onLinkSubmit(x)}
+              className="primary-btn btn--lg btn--move"
+            >
+              Shorten It!
+            </span>
+          </form>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+}
+ShortenLnk.propTypes = {
+  onLinkSubmit: Proptypes.func.isRequired
 };
-
 export default ShortenLnk;
