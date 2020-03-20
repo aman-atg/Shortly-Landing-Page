ced2007cbe854f17c68be2153c5be7a2f672bc25
aman-atg <amanansari.a2@gmail.com>
2020-03-20 21:22:24 +0530

    implement urlSchema + create new err state

--- src/components/Links/Links.jsx
+++ src/components/Links/Links.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
 import ShortenLnk from "../Header/ShortenLnk";
+import UrlSchema from "../../apis/rel-ink-api";
+
 class Links extends Component {
-  state = { links: [] }; //dlt link
-  //NEEEEEEEED TO MANAGE ERROR FIRST..
+  state = { links: [], err: false };
+
   /// define prop to call that api from api import
   async getHash(url) {
     // see what would happen without async
@@ -20,13 +22,21 @@ class Links extends Component {
   }
 
   onLinkSubmit = (url, e) => {
+    // ======== >> CHECKING FOR ERR << ==========
+    try {
+      UrlSchema.validate({ url });
+      this.setState({ err: false });
+    } catch {
+      this.setState({ err: true });
+    }
+
     if (e !== undefined) e.preventDefault();
     const hashId = this.getHash(url);
     this.setState({ links: [{ url, hashId }, ...this.state.links] });
   };
 
   render() {
-    return <ShortenLnk onLinkSubmit={this.onLinkSubmit} />;
+    return <ShortenLnk onLinkSubmit={this.onLinkSubmit} err={this.state.err} />;
   }
 }
 
