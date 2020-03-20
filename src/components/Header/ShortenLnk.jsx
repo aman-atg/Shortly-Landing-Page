f5d1eb46cf3c79ed634a791759afc437f5a8b9da
aman-atg <amanansari.a2@gmail.com>
2020-03-20 16:01:25 +0530

    make changes

--- src/components/Header/ShortenLnk.jsx
+++ src/components/Header/ShortenLnk.jsx
@@ -3,27 +3,30 @@ import Proptypes from "prop-types";
 //make it a class
 class ShortenLnk extends React.Component {
   state = {
-    url: ""
+    url: "",
+    err: ""
   };
-
+  /// create simple-schema and
+  // manage errors
   render() {
     const { props, state } = this;
-
     return (
       <div className="wrapper">
         <div className="shorten-lnk box">
           <form
             className="shorten-lnk_Input-group"
-            onSubmit={() => props.onLinkSubmit()}
+            onSubmit={e => props.onLinkSubmit(state.url, e)}
           >
             <input
               type="text"
               name="link"
               id="link"
               placeholder="Shorten a link here..."
+              onChange={e => this.setState({ url: e.target.value })}
+              value={state.url}
             />
             <span
-              onClick={x => props.onLinkSubmit(x)}
+              onClick={() => props.onLinkSubmit(state.url)}
               className="primary-btn btn--lg btn--move"
             >
               Shorten It!
