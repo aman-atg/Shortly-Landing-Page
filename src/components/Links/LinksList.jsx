ebc045c69771ba08e3c0ec635395358be8127fc1
aman-atg <amanansari.a2@gmail.com>
2020-03-21 18:36:00 +0530

    using uuid for keys

--- src/components/Links/LinksList.jsx
+++ src/components/Links/LinksList.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import uuid from "uuid/v4";
 import Link from "./Link";
 const LinksList = props => {
   const { links } = props;
   return (
     <div className="linksList">
       {links.map(link => (
-        <Link link={link} />
+        <Link key={uuid()} link={link} />
       ))}
     </div>
   );
