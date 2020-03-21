40495bb7498d6c94ca7740e8067e8f15f5dfa231
aman-atg <amanansari.a2@gmail.com>
2020-03-21 16:30:10 +0530

    Link comp complete: just need to implement copy fn

--- src/components/Links/Link.jsx
+++ src/components/Links/Link.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
 const Link = props => {
+  // need to add clipboardjs for copy
+  // need to maintain copy or not copy state or i can just implement copy => copied then nthing...
   const { link } = props;
   const { url, hashId } = link;
   const shortUrl = "https://rel.ink.com/" + hashId;
   return (
     <div className="link-container">
-      <p>{url}</p> <p>{shortUrl}</p>
+      <div className="url">{url}</div>
+      <div className="link-container_ShortUrl">
+        <p className="shortUrl">{shortUrl}</p>
+        <p className="primary-btn">Copied!</p>
+      </div>
     </div>
   );
 };
