23f2c193866518da09a5c1ebf8b8873dd983c9c8
aman-atg <amanansari.a2@gmail.com>
2020-03-21 22:46:11 +0530

    sfc -> cc and  add clipboard

--- src/components/Links/Link.jsx
+++ src/components/Links/Link.jsx
@@ -1,22 +1,51 @@
 import React from "react";
+import Clipboard from "clipboard";
+class Link extends React.Component {
+  state = {
+    copyBtnValue: "Copy"
+  };
+  copyBtn = React.createRef();
+  componentDidMount() {
+    this.copyClipboard = new Clipboard(this.copyBtn.current);
+    this.copyClipboard
+      .on("error", () => {
+        alert("Something is wrong with your browser.");
+      })
+      .on("success", () => {
+        this.setState({ copyBtnValue: "Copied!" });
+        this.copyBtn.current.id = "copied";
+        setTimeout(() => {
+          this.setState({ copyBtnValue: "Copy" });
+          this.copyBtn.current.id = "";
+        }, 3500);
+      });
+  }
+  componentWillUnmount() {
+    this.copyClipboard.destroy();
+  }
+  //
 
-const Link = props => {
-  // need to add clipboardjs for copy
-  // need to maintain copy or not copy state or i can just implement copy => copied then nthing...
-  const { link } = props;
-  const { url, hashId } = link;
-  const shortUrl = "https://rel.ink.com/" + hashId;
-  return (
-    <div className="link-container">
-      <div className="url">{url}</div>
-      <div className="link-container_ShortUrl">
-        <p className="shortUrl">{shortUrl}</p>
-        <p tabIndex={1} className="primary-btn">
-          Copied!
-        </p>
+  render() {
+    const { state, props } = this;
+    const { link } = props;
+    const { url, hashId } = link;
+    const shortUrl = "https://rel.ink.com/" + hashId;
+    return (
+      <div className="link-container">
+        <div className="url">{url}</div>
+        <div className="link-container_ShortUrl">
+          <p className="shortUrl">{shortUrl}</p>
+          <p
+            tabIndex={1}
+            className="primary-btn"
+            ref={this.copyBtn}
+            data-clipboard-text={shortUrl}
+          >
+            {state.copyBtnValue}
+          </p>
+        </div>
       </div>
-    </div>
-  );
-};
-
+    );
+  }
+}
 export default Link;
