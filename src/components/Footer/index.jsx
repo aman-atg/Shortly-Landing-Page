ce2da231c10a14c5ba32cb3b5d0d32aec8fa086a
aman-atg <amanansari.a2@gmail.com>
2020-03-22 16:53:17 +0530

    add Sign comp.

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import FooterHeading from "./FooterHeading";
 import Footer from "./Footer";
+import Sign from "./Sign";
 
 const FooterIndex = () => {
   return (
     <div className="footerIndex">
       <FooterHeading />
       <Footer />
+      <Sign />
     </div>
   );
 };
