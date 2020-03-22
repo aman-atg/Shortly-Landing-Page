e6c907d9589dc923004cb8f0e76fb6e2a69650d9
aman-atg <amanansari.a2@gmail.com>
2020-03-22 16:53:04 +0530

    style and comp. added for sign

---
+++ src/components/Footer/Sign.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Sign = () => {
+  return (
+    <div className="sign">
+      Challenge by{" "}
+      <a href="https://frontendmentor.io" target="_blank">
+        FrontEndMentor
+      </a>{" "}
+      Coded by{" "}
+      <a href="https://github.com/aman-atg" target="_blank">
+        Aman Ansari{" "}
+      </a>
+      With <i class="fas fa-heart"></i> And <i class="fas fa-mug-hot"> </i>
+    </div>
+  );
+};
+
+export default Sign;
