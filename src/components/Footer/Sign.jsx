06d4f264ee4d2437ffb4dc4a6c3f87a8e9e02773
aman-atg <amanansari.a2@gmail.com>
2020-03-22 19:07:31 +0530

    remove security risks

--- src/components/Footer/Sign.jsx
+++ src/components/Footer/Sign.jsx
@@ -4,11 +4,19 @@ const Sign = () => {
   return (
     <div className="sign">
       Challenge by{" "}
-      <a href="https://frontendmentor.io" target="_blank">
+      <a
+        href="https://frontendmentor.io"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         FrontEndMentor
       </a>{" "}
       Coded by{" "}
-      <a href="https://github.com/aman-atg" target="_blank">
+      <a
+        href="https://github.com/aman-atg"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         Aman Ansari{" "}
       </a>
       With <i className="fas fa-heart"></i> And{" "}
